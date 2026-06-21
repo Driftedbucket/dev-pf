07fa4c6bd775ac6906040b0a4ec8a61b4bc00c7a
Driftedbucket <ndinowix@gmail.com>
2026-06-21 23:00:48 +0200

    feat: add classNames and language option

--- app/components/Topbar.js
+++ app/components/Topbar.js
@@ -4,16 +4,17 @@ import styles from './Topbar.module.css'
 
 export default function Topbar(){
     return(
-        <header>
-            <div>
+        <header className={styles.topbar}>
+            <div className={styles.socials}>
                 <a href="https://wa.me/264816752591" target="_blank" rel="noreferrer"><FaWhatsapp/></a>
                 <a href="https://github.com/Driftedbucket" target="_blank" rel="noreferrer"><FaGithub/></a>
                 <a href="https://www.linkedin.com/in/awike-gulu-2a409a3a5/" target="_blank" rel="noreferrer"><FaLinkedin/></a>
                 <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebook/></a>
                 <a href="https://instagram.com/awike_.rna" target="_blank" rel="noreferrer"><FaInstagram/></a>
+                <span className={styles.lang}>EN</span>
             </div>
 
-            <a href="/cv.pdf" download >
+            <a href="/cv.pdf" download className={styles.cvButton}>
                 CV <HiOutlineDownload/>
             </a>
         </header>
